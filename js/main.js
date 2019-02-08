85e42cc09a5073d73a0ac786d88854f0e36de978
cesar18pena <cesar18pena@gmail.com>
2019-02-07 21:02:03 -0400

    feat: improve the responsive design and make webpage with more accesibility

--- js/main.js
+++ js/main.js
@@ -161,10 +161,12 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `restaurant: ${restaurant.name}`;
   li.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+  name.setAttribute('tabindex', '0');
   li.append(name);
 
   const neighborhood = document.createElement('p');
@@ -178,6 +180,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label', `Details about ${restaurant.name}`)
   li.append(more)
 
   return li
